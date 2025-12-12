3c929ca43a85dd7059e9ccc013f93f9eea744375
ArsenZar <arsenzar@gmail.com>
2025-12-12 14:44:08 +0200

    form typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
+// Імпортуємо вбудований тип FormikHelpers
 import { Formik, Form, Field } from "formik";
+import type { FormikHelpers } from "formik";
+
+
+interface OrderFormValues {
+  username: string;
+  email: string;
+}
+
+const initialValues: OrderFormValues = {
+  username: "",
+  email: "",
+};
 
 export default function OrderForm() {
+  const handleSubmit = (
+    values: OrderFormValues,
+    actions: FormikHelpers<OrderFormValues>
+  ) => {
+    console.log("Order data:", values);
+    actions.resetForm();
+  };
+
   return (
-    <Formik
-      initialValues={{
-        username: "hello",
-        email: ""
-      }}
-      onSubmit={() => { }}
-    >
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       <Form>
         <Field type="text" name="username" />
-        <Field type="email" name="email" />
+        <Field type="text" name="email" />
         <button type="submit">Place order</button>
       </Form>
     </Formik>
